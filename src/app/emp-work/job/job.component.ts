45a63f8c2dc8e15ef44d841b84ef355101e60942
shika <loka.eslam@hotmail.com>
2021-05-23 14:01:36 +0200

    fix: fix job description in mobile viewport

--- src/app/emp-work/job/job.component.ts
+++ src/app/emp-work/job/job.component.ts
@@ -4,7 +4,7 @@ import { JobDesc } from 'src/app/core/models/job.model';
 @Component({
   selector: 'app-job',
   templateUrl: './job.component.html',
-  styleUrls: ['./job.component.scss']
+  styleUrls: ['./job.component.scss', "../../../assets/scss/media/_job.scss"]
 })
 export class JobComponent implements OnInit {
   @Input() job: JobDesc;
